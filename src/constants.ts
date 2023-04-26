bb1b0e8cdd98a8e50e1e5c18ae77fdfe434b37eb
Mack Ignacio <mack_ignacio@yahoo.com>
2023-04-26 10:53:36 +0800

    Add URL constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,10 @@
 export const ACTIONS = {
   COUNTRY: "country",
+  INFO: "country_info",
   CAPITAL: "capital",
   CONTINENT: "continent",
 };
+
+export const URL_REST_COUNTRIES = "https://restcountries.com/v3.1";
+export const URL_WIKIPEDIA =
+  "https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=";
