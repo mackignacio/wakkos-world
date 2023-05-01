0b283e70e36e7d4db5ff0f96c4e33b848ce958c6
Mack Ignacio <mack_ignacio@yahoo.com>
2023-05-01 08:28:39 +0800

    Reformat divider block

--- src/message.ts
+++ src/message.ts
@@ -11,27 +11,21 @@ export const initial = async ({ message, say }: any) => {
           text: `:wave:  Hello, <@${message.user}>. This is your *#1* assistant Wakko Warner. You can ask me anything you want to know about the countries of the world!!! \n\n *Please select a question:*`,
         },
       },
-      {
-        type: "divider",
-      },
+      { type: "divider" },
       question.input(
         ACTIONS.COUNTRY,
         "Enter country name",
         "What is the country of?"
       ),
       question.actions(ACTIONS.COUNTRY, "Ask me"),
-      {
-        type: "divider",
-      },
+      { type: "divider" },
       question.input(
         ACTIONS.CAPITAL,
         "Enter country's capital",
         "What country has the capital of?"
       ),
       question.actions(ACTIONS.CAPITAL, "Ask me"),
-      {
-        type: "divider",
-      },
+      { type: "divider" },
       question.input(
         ACTIONS.CONTINENT,
         "Enter continent name",
